c90afcc4f260a2586bb123881f600abfef47f550
josefinkl <josefin.klangefjall@gmail.com>
2024-11-19 18:16:13 +0100

    Added computer as Player2

--- src/Game.java
+++ src/Game.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Random;
 
 public class Game {
     private String player;
@@ -28,8 +29,23 @@ public class Game {
         System.out.println("Write Player 1 name (X):");
         namePlayer1 = scanner.nextLine();
 
-        System.out.println("Write Player 2 name (O):");
-        namePlayer2 = scanner.nextLine();
+        System.out.println("Do you want to play with computer (Y/N)?");
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equalsIgnoreCase("N")) {
+                System.out.println("Write Player 2 name (O):");
+                namePlayer2 = scanner.nextLine();
+                break;
+
+            }
+            if (!input.equalsIgnoreCase("Y")) {
+                System.out.println("Write Y/N");
+            } else {
+                namePlayer2 = "Computer";
+                break;
+            }
+
+        }
         play(0,0, namePlayer1, namePlayer2);
     }
 
@@ -49,73 +65,96 @@ public class Game {
                 board.printBoard();
                 Scanner scanner = new Scanner(System.in);
                 String input = scanner.next();
-                if (board.availableNumber(Integer.parseInt(input))) {
-                    if (Integer.parseInt(input) >= 1 && Integer.parseInt(input) <= 9) {
-                        board.set(Integer.parseInt(input), "X");
-                        board.endGame();
+                if (Integer.parseInt(input) != 0) {
+                    if (board.availableNumber(Integer.parseInt(input))) {
+                        if (Integer.parseInt(input) >= 1 && Integer.parseInt(input) <= 9) {
+                            board.set(Integer.parseInt(input), "X");
+                            board.endGame();
 
 
-                        if (board.getResult() == "Win") {
-                            System.out.println(namePlayer1 + " " + board.getResult());
-                            numberOfWinPlayer1++;
-                            System.out.println(namePlayer1 + " has won " + numberOfWinPlayer1 + " time(s).");
-                            if (numberOfWinPlayer1 == 3){
-                                System.out.println(namePlayer1 + " came first to 3 won games!");
-                                break;
+                            if (board.getResult() == "Win") {
+                                System.out.println(namePlayer1 + " " + board.getResult());
+                                numberOfWinPlayer1++;
+                                System.out.println(namePlayer1 + " has won " + numberOfWinPlayer1 + " time(s).");
+                                if (numberOfWinPlayer1 == 3) {
+                                    System.out.println(namePlayer1 + " came first to 3 won games!");
+                                    break;
+                                }
+                                continuePlay = false;
+
+                                new Game().play(numberOfWinPlayer1, numberOfWinPlayer2, namePlayer1, namePlayer2);
+                            } else if (board.getResult() == "Tie") {
+                                System.out.println("Tie");
+                                continuePlay = false;
                             }
-                            continuePlay = false;
-
-                            new Game().play(numberOfWinPlayer1, numberOfWinPlayer2, namePlayer1, namePlayer2);
-                        } else if (board.getResult() == "Tie") {
-                            System.out.println("Tie");
-                            continuePlay = false;
+                            player = namePlayer2;
+                        } else {
+                            System.out.println("pick a number:");
                         }
-                        player = namePlayer2;
-                    }
-
-                    else {
-                        System.out.println("pick a number:");
-                    }
                     } else {
                         System.out.println("pick an available number:");
                     }
 
                 }
-
-            else if (player == namePlayer2) {
-                System.out.println(namePlayer2 + " to play, pick a number:");
-                board.printBoard();
-                Scanner scanner = new Scanner(System.in);
-                String input = scanner.next();
-                if (board.availableNumber(Integer.parseInt(input))) {
-                    if (Integer.parseInt(input) >= 1 && Integer.parseInt(input) <= 9) {
-                        board.set(Integer.parseInt(input), "O");
-                        board.endGame();
-
-
-                        if (board.getResult() == "Win") {
-                            System.out.println(namePlayer2 + " " + board.getResult());
-                            numberOfWinPlayer2++;
-                            System.out.println(namePlayer2 + " has won " + numberOfWinPlayer2 + " time(s).");
-                            if (numberOfWinPlayer2 == 3){
-                                System.out.println(namePlayer2 + " came first to 3 won games!");
-                                break;
-                            }
-                            continuePlay = false;
-                            new Game().play(numberOfWinPlayer1, numberOfWinPlayer2, namePlayer1, namePlayer2);
-                        }
-                        else if (board.getResult() == "Tie") {
-                            System.out.println("Tie");
-                        }
-                        player = namePlayer1;
-                    } else {
-                        System.out.println("pick a number:");
-                    }
-                }
                 else {
                     System.out.println("pick an available number:");
                 }
+            }
 
+            else if (player == namePlayer2) {
+                String input;
+
+                if(namePlayer2 != "Computer") {
+                    System.out.println(namePlayer2 + " to play, pick a number:");
+                    board.printBoard();
+                    Scanner scanner = new Scanner(System.in);
+                    input = scanner.next();
+                }
+                else {
+                    Random rand = new Random();
+                    Integer n = rand.nextInt(9);
+                    input = Integer.toString(n);
+                    System.out.println(input);
+
+                }
+                if(Integer.parseInt(input) != 0) {
+                    if (board.availableNumber(Integer.parseInt(input))) {
+                        if (Integer.parseInt(input) >= 1 && Integer.parseInt(input) <= 9) {
+                            board.set(Integer.parseInt(input), "O");
+                            board.endGame();
+
+
+                            if (board.getResult() == "Win") {
+                                System.out.println(namePlayer2 + " " + board.getResult());
+                                numberOfWinPlayer2++;
+                                System.out.println(namePlayer2 + " has won " + numberOfWinPlayer2 + " time(s).");
+                                if (numberOfWinPlayer2 == 3) {
+                                    System.out.println(namePlayer2 + " came first to 3 won games!");
+                                    break;
+                                }
+                                continuePlay = false;
+                                new Game().play(numberOfWinPlayer1, numberOfWinPlayer2, namePlayer1, namePlayer2);
+                            } else if (board.getResult() == "Tie") {
+                                System.out.println("Tie");
+                            }
+                            player = namePlayer1;
+                        } else {
+                            if(namePlayer2 != "Computer") {
+                                System.out.println("pick a number:");
+                            }
+
+                        }
+                    } else {
+                        if(namePlayer2 != "Computer") {
+                            System.out.println("pick an available number:");
+                        }
+                    }
+                }
+                else {
+                    if(namePlayer2 != "Computer") {
+                        System.out.println("pick an available number:");
+                    }
+                }
             }
 
             }
